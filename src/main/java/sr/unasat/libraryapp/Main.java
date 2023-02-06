ff342fd472833d2beef4e21c473d63ae54a51082
Chanella <c.chaaajong@unasat.sr>
2023-02-06 20:06:18 -0300

    Aangepast

---
+++ src/main/java/sr/unasat/libraryapp/Main.java
@@ -0,0 +1,11 @@
+package sr.unasat.libraryapp;
+
+import sr.unasat.libraryapp.app.Application;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Application.Menu();
+    }
+}
